7adef67717b3a862c4c1618627de0b2f7bbc1621
Rodrigo Ramos da Silva <a35479@alunos.ipb.pt>
2021-10-18 11:54:05 +0100

    Initial commit

--- src/main/java/com/pet/manager/Application.java
+++ src/main/java/com/pet/manager/Application.java
@@ -2,11 +2,11 @@ package com.pet.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.util.BsonUtils;
 
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
